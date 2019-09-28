06a97859dc73331630c9d1d61b6bb926c28efddf
bethurban <beth.urban@gmail.com>
2019-09-28 11:11:21 -0400

    Set initial state in Blog, fetch blog API after mounting, render blog title/description

--- src/containers/Blog.js
+++ src/containers/Blog.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
 
 class Blog extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { blog: {} }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/blogs/1')
+      .then(resp => resp.json())
+      .then(json => {
+        this.setState({
+          blog: json
+        })
+      })
+  }
+
   render() {
     return (
-      <h1>Beth Codes</h1>
+      <div id="blog">
+        <h1>{this.state.blog.title}</h1>
+        <div id="description">
+          <p>{this.state.blog.description}</p>
+        </div>
+      </div>
     )
   }
 
