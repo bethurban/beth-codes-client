4a9be3973f798f4564b5a58ea777fc85437dfe91
bethurban <beth.urban@gmail.com>
2019-10-18 11:42:55 -0400

    Build new post form on New.

--- src/containers/New.js
+++ src/containers/New.js
@@ -4,7 +4,10 @@ class New extends Component {
 
   constructor(props){
     super(props);
-    this.state = { blog: {} }
+    this.state = {
+      title: "Title",
+      content: "Content"
+    }
   }
 
   // componentDidMount() {
@@ -17,10 +20,42 @@ class New extends Component {
   //     })
   // }
 
+  handleChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.title, this.state.content)
+  }
+
   render() {
     return (
       <div id="form">
-        Hello!
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <input
+              required
+              type="text"
+              onChange={this.handleChange}
+              name="title"
+              value={this.state.title}
+            />
+          </div>
+          <div>
+            <textarea
+              required
+              type="text"
+              onChange={this.handleChange}
+              name="content"
+              value={this.state.content}
+            />
+          </div>
+          <button type="submit">Publish</button>
+        </form>
       </div>
     )
   }
